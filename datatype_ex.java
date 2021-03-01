7762bf8b49b364b79acec0de8aac734ec0144c44
PiriyankaSelvarajah <77662143+PiriyankaSelvarajah@users.noreply.github.com>
2021-03-01 20:20:51 +0530

    Update and rename atatype_ex.java to datatype_ex.java

---
+++ datatype_ex.java
@@ -0,0 +1,32 @@
+public class datatype_ex {
+    public static void main(String[] args){
+        //byte a whole number between -128 and 127.
+        byte num=113;
+        System.out.println(num);
+
+        //short Its range is -32,768 to 32767.
+        short num2=5684;
+        System.out.println(num2);
+
+        //long ranging from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807.size: 8 bytes
+        long num3=-123322526265L;
+        System.out.println(num3);
+
+        //double 15 decimal digits. size: 8 bytes
+        double num4=234.3556;
+        System.out.println(num4);
+
+        //float Sufficient for holding 6 to 7 decimal digits
+        float num5=19.98f;
+        System.out.println(num5);
+        
+        //boolean True or False
+        boolean num6=false;
+        System.out.println(num6);
+        
+         //char holds characters.
+        char ch='A';
+        System.out.println(ch);
+        
+    }
+}
